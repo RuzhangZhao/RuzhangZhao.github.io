03f554c07f7ce83698a5cd1e5868f642cf0e7859
zrz15 <30921703+zrz15@users.noreply.github.com>
2022-03-21 19:09:55 -0400

    update

--- file/multiSNPtmp.R
+++ file/multiSNPtmp.R
@@ -1,14 +1,14 @@
 
 print("JS2")
-cur_iter<-5
+cur_iter<-1
 #Nonnull_index<-c(2,130,173)
-Nonnull_index<-c(2,130,192)
+#Nonnull_index<-c(2,130,192)
 #Nonnull_index<-c(139,151,211)
 
 #Nonnull_index<-c(151,139,103)
 #Nonnull_index<-sample(1:254,3)
 #Nonnull_index<-c(11,115,227)#
-#Nonnull_index<-c(25,83,196)
+Nonnull_index<-c(25,83,196)
 library(inline,quietly = T)
 library(data.table,quietly = T)
 library(dplyr,quietly = T)
@@ -286,7 +286,7 @@ EAF[Nonnull_index]
   #a<-adaptiveGMMlasso2(UKBB_pop_all,N_SNP,study_info)
   a<-adaptiveGMMlasso4(UKBB_pop_all,N_SNP,study_info_scaled)
   lasgw_pos<-a$pos
-  print(paste0("Only GWAS: len:",length(lasgw_pos),", true select:",sum(lasgw_pos%in%Nonnull_index_filter_less)))
+  print(paste0("adaptiveGMMlasso: len:",length(lasgw_pos),", true select:",sum(lasgw_pos%in%Nonnull_index_filter_less)))
   
   if(0){
   UKBB_full_fit2 <-cv.glmnet(x=UKBB_pop_all[,var_names_full_fit],y=UKBB_pop_all[,"Y"])
