3dcd853254e05ce9a17e9c12644d8cb249de72ec
zrz15 <30921703+zrz15@users.noreply.github.com>
2022-03-22 16:09:52 -0400

    update

--- file/adaptiveGMMLasso.R
+++ file/adaptiveGMMLasso.R
@@ -460,7 +460,7 @@ adaptiveGMMlasso21<-function(UKBB_pop,study_info,cor_cut=0.5){
   )
 }
 
-adaptiveGMMlasso35<-function(UKBB_pop,study_info,cor_cut=0.5,filter_index=TRUE){
+adaptiveGMMlasso35<-function(UKBB_pop,study_info,cor_cut=0.5,filter_index=TRUE,p_val_cut=1e-5){
   if(filter_index){
     ### Trick 1 index filtering 
     index_filter<-c()
