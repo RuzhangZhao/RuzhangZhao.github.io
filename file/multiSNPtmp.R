f4297777e0c7c5f168221d550ae4d7cc83db0420
zrz15 <30921703+zrz15@users.noreply.github.com>
2022-03-21 20:29:18 -0400

    update

--- file/multiSNPtmp.R
+++ file/multiSNPtmp.R
@@ -1,6 +1,6 @@
 
 print("JS2")
-cur_iter<-1
+cur_iter<-8
 #Nonnull_index<-c(2,130,173)
 Nonnull_index<-c(2,130,192)
 #Nonnull_index<-c(139,151,211)
@@ -8,7 +8,7 @@ Nonnull_index<-c(2,130,192)
 #Nonnull_index<-c(151,139,103)
 #Nonnull_index<-sample(1:254,3)
 #Nonnull_index<-c(11,115,227)#
-#Nonnull_index<-c(25,83,196)
+Nonnull_index<-c(25,83,196)
 library(inline,quietly = T)
 library(data.table,quietly = T)
 library(dplyr,quietly = T)
