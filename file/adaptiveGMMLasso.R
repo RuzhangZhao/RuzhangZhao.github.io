1d0ab42590f3f54a8304b48f88d40f11dc99a4a9
zrz15 <30921703+zrz15@users.noreply.github.com>
2022-03-22 09:17:03 -0400

    update

--- file/adaptiveGMMLasso.R
+++ file/adaptiveGMMLasso.R
@@ -7,7 +7,9 @@ if(0){
 ## Both SNP and phenotype should be centered. 
 library(expm,quietly = T)
 library(magic,quietly = T)
-adaptiveGMMlasso<-function(UKBB_pop,N_SNP,study_info){
+adaptiveGMMlasso<-function(UKBB_pop,study_info){
+  N_SNP<-ncol(UKBB_pop)-1
+  colnames(UKBB_pop)[-1]<-paste0("SNP",1:(N_SNP))
   colnames(UKBB_pop)[1]<-"Y"
   var_SNP<-paste0("SNP",1:(N_SNP))
   len_SNP<-length(var_SNP)
@@ -142,8 +144,9 @@ adaptiveGMMlasso<-function(UKBB_pop,N_SNP,study_info){
 ## Both SNP and phenotype should be centered. 
 library(expm,quietly = T)
 library(magic,quietly = T)
-adaptiveGMMlasso35<-function(UKBB_pop,N_SNP,study_info){
+adaptiveGMMlasso2<-function(UKBB_pop,study_info){
   N_SNP<-ncol(UKBB_pop)-1
+  colnames(UKBB_pop)[-1]<-paste0("SNP",1:(N_SNP))
   colnames(UKBB_pop)[1]<-"Y"
   var_SNP<-paste0("SNP",1:(N_SNP))
   len_SNP<-length(var_SNP)
