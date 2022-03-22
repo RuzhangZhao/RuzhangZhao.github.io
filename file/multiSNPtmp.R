193172b0637618d0ca246c3e1008f2d0058b0505
zrz15 <30921703+zrz15@users.noreply.github.com>
2022-03-21 21:46:13 -0400

    update

--- file/multiSNPtmp.R
+++ file/multiSNPtmp.R
@@ -8,7 +8,7 @@ Nonnull_index<-c(2,130,192)
 #Nonnull_index<-c(151,139,103)
 #Nonnull_index<-sample(1:254,3)
 #Nonnull_index<-c(11,115,227)#
-Nonnull_index<-c(25,83,196)
+#Nonnull_index<-c(25,83,196)
 library(inline,quietly = T)
 library(data.table,quietly = T)
 library(dplyr,quietly = T)
@@ -282,10 +282,10 @@ EAF[Nonnull_index]
   source("~/multiSNP/adaptiveGMMLasso.R")
   #a<-adaptiveGMMlasso2(UKBB_pop_all,N_SNP,study_info_scaled)
   #a3<-adaptiveGMMlasso3(UKBB_pop_all,N_SNP,study_info_scaled)
-  #a31<-adaptiveGMMlasso31(UKBB_pop_all,N_SNP,study_info_scaled,type=3,cor_cut = 0.5)
+  a31<-adaptiveGMMlasso31(UKBB_pop_all,N_SNP,study_info_scaled,type=3,cor_cut = 0.5)
   #a<-adaptiveGMMlasso2(UKBB_pop_all,N_SNP,study_info_scaled)
-  a<-adaptiveGMMlasso34(UKBB_pop_all,N_SNP,study_info_scaled,cor_cut = 0.6,type=3)
-  a<-adaptiveGMMlasso35(UKBB_pop_all,study_info_scaled,cor_cut = 0.6,type=3)
+  a<-adaptiveGMMlasso34(UKBB_pop_all,study_info_scaled,cor_cut = 0.6,type=3)
+  a<-adaptiveGMMlasso35(UKBB_pop_all,study_info_scaled,cor_cut = 0.5,type=3)
   lasgw_pos<-a$pos
   print(paste0("adaptiveGMMlasso: len:",length(lasgw_pos),", true select:",sum(lasgw_pos%in%Nonnull_index_filter_less)))
   
