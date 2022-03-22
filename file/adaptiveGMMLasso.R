bc7d193b0cf7535e4726320bf6f34e5b4d020e58
zrz15 <30921703+zrz15@users.noreply.github.com>
2022-03-22 07:48:04 -0400

    update

--- file/adaptiveGMMLasso.R
+++ file/adaptiveGMMLasso.R
@@ -1587,7 +1587,7 @@ adaptiveGMMlasso35<-function(UKBB_pop,study_info,type=3,filter_index = TRUE,cor_
   W_nonzero = W[index_nonzero,index_nonzero]
   
   final_v<-diag(inv_Sigsum_scaled_nonzero%*%W_nonzero%*%inv_Sigsum_scaled_nonzero)
-  
+  final_v<-diag(inv_Sigsum_scaled_nonzero)
   aa_final<-1-pchisq(N_Pop*beta[index_nonzero]^2/final_v,1)
   if(filter_index){
     pos<-index_filter[index_nonzero[which(aa_final<0.05/ncol(UKBB_pop))]]
